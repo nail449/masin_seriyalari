a271aa55af44bc3af460da9066cd45dff3a32ac2
nail449 <81387417+nail449@users.noreply.github.com>
2021-05-29 12:39:04 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+
+namespace masin_seriyalari
+{
+    class masin
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oyrenmek istediyiniz  masin seriyasini yazin: ");
+            string seriyalar = Console.ReadLine();
+
+            switch (seriyalar)
+            {
+                case "01":
+                    Console.WriteLine("Absheron");
+                    break;
+                case "02":
+                    Console.WriteLine("Agdam");
+                    break;
+                case "03":
+                    Console.WriteLine("Agdash");
+                    break;
+                case "04":
+                    Console.WriteLine("Agcabedi");
+                    break;
+                case "05":
+                    Console.WriteLine("Agstafa");
+                    break;
+                case "06":
+                    Console.WriteLine("Agsu");
+                    break;
+                case "07":
+                    Console.WriteLine("Astara");
+                    break;
+                case "08":
+                    Console.WriteLine("Balaken");
+                    break;
+                case "09":
+                    Console.WriteLine("Berde");
+                    break;
+                case "10":
+                    Console.WriteLine("Baki");
+                    break;
+                case "11":
+                    Console.WriteLine("Beyleqan");
+                    break;
+                case "12":
+                    Console.WriteLine("Bilesuvar");
+                    break;
+                case "14":
+                    Console.WriteLine("Cebrayil");
+                    break;
+                case "15":
+                    Console.WriteLine("Celilabad");
+                    break;
+                case "16":
+                    Console.WriteLine("Daskesen");
+                    break;
+                case "17":
+                    Console.WriteLine("Deveci");
+                    break;
+                case "18":
+                    Console.WriteLine("Shirvan");
+                    break;
+                case "19":
+                    Console.WriteLine("Fuzuli");
+                    break;
+                case "20":
+                    Console.WriteLine("Gence");
+                    break;
+                case "21":
+                    Console.WriteLine("Gedebey");
+                    break;
+                case "22":
+                    Console.WriteLine("Goranboy");
+                    break;
+                case "23":
+                    Console.WriteLine("Goycay");
+                    break;
+                case "24":
+                    Console.WriteLine("Haciqabul");
+                    break;
+                case "25":
+                    Console.WriteLine("Xanlar");
+                    break;
+                case "26":
+                    Console.WriteLine("Xankendi");
+                    break;
+                case "27":
+                    Console.WriteLine("Xachmaz");
+                    break;
+                case "28":
+                    Console.WriteLine("Xocavend");
+                    break;
+                case "29":
+                    Console.WriteLine("Xizi");
+                    break;
+                case "30":
+                    Console.WriteLine("Imishli");
+                    break;
+                case "31":
+                    Console.WriteLine("Ismayilli");
+                    break;
+                case "32":
+                    Console.WriteLine("Kelbecer");
+                    break;
+                case "33":
+                    Console.WriteLine("Kurdemir");
+                    break;
+                case "34":
+                    Console.WriteLine("Qax");
+                    break;
+                case "35":
+                    Console.WriteLine("Qazax");
+                    break;
+                case "36":
+                    Console.WriteLine("Qebele");
+                    break;
+                case "37":
+                    Console.WriteLine("Qobustan");
+                    break;
+                case "38":
+                    Console.WriteLine("Qusar");
+                    break;
+                case "39":
+                    Console.WriteLine("Qubadli");
+                    break;
+                case "40":
+                    Console.WriteLine("Quba");
+                    break;
+                case "41":
+                    Console.WriteLine("Lachin");
+                    break;
+                case "42":
+                    Console.WriteLine("Lenkeran");
+                    break;
+                case "43":
+                    Console.WriteLine("Lerik");
+                    break;
+                case "44":
+                    Console.WriteLine("Masalli");
+                    break;
+                case "45":
+                    Console.WriteLine("Mingecevir");
+                    break;
+                case "46":
+                    Console.WriteLine("Naftalan");
+                    break;
+                case "47":
+                    Console.WriteLine("Neftcala");
+                    break;
+                case "48":
+                    Console.WriteLine("Oguz");
+                    break;
+                case "49":
+                    Console.WriteLine("Saatli");
+                    break;
+                case "50":
+                    Console.WriteLine("Sumqayit");
+                    break;
+                case "51":
+                    Console.WriteLine("Samux");
+                    break;
+                case "52":
+                    Console.WriteLine("Salyan");
+                    break;
+                case "53":
+                    Console.WriteLine("Siyezen");
+                    break;
+                case "54":
+                    Console.WriteLine("Sabirabad");
+                    break;
+                case "55":
+                    Console.WriteLine("Sheki");
+                    break;
+                case "56":
+                    Console.WriteLine("Shamaxi");
+                    break;
+                case "57":
+                    Console.WriteLine("Shemkir");
+                    break;
+                case "58":
+                    Console.WriteLine("Susa");
+                    break;
+                case "59":
+                    Console.WriteLine("Terter");
+                    break;
+                case "60":
+                    Console.WriteLine("Tovuz");
+                    break;
+                case "61":
+                    Console.WriteLine("Ucar");
+                    break;
+                case "62":
+                    Console.WriteLine("Zaqatala");
+                    break;
+                case "63":
+                    Console.WriteLine("Zerdab");
+                    break;
+                case "64":
+                    Console.WriteLine("Zengilan");
+                    break;
+                case "65":
+                    Console.WriteLine("Yardimli");
+                    break;
+                case "66":
+                    Console.WriteLine("Yevlax");
+                    break;
+                case "67":
+                    Console.WriteLine("Babek");
+                    break;
+                case "68":
+                    Console.WriteLine("Sherur");
+                    break;
+                case "70":
+                    Console.WriteLine("Naxchivan Seheri");
+                    break;
+                case "71":
+                    Console.WriteLine("Shahbuz");
+                    break;
+                case "72":
+                    Console.WriteLine("Culfa");
+                    break;
+                case "85":
+                    Console.WriteLine("Naxcivan");
+                    break;
+                case "90":
+                    Console.WriteLine("Baki");
+                    break;
+                case "99":
+                    Console.WriteLine("Baki");
+                    break;
+                default:
+                    Console.WriteLine("Tessufki bu seriya yoxdur.");
+                    break;
+            }
+
+        }
+    }
+}
+
+
+
